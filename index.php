eb6e3b292a72b178a66d976bbe3b30af100775a2
milo <milosorelcpt@gmail.com>
2025-11-25 09:20:49 +0100

    commit temp

--- index.php
+++ index.php
@@ -8,17 +8,14 @@
 </head>
 <body>
 
-    <header>
-        <h1>Welcome to My livre dont vous etres le hero</h1>
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+    <div>
+        <p>menubar en dessous</p>
+        <?php
+        if (!include('src/php/comp/header.php')) {
+            echo "<p style='color:red;'>ERREUR : FICHIER header.php INTROUVABLE</p>";
+        }
+        ?>
+    </div>
 
     <main>
         <section id="home">
---
+++ pages/creationTest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Index Page</title>
+    <link rel="stylesheet" href="src/css/styles.css">
+</head>
+<body>
+
+    <main>
+        <h1>form creation heros</h1>
+    </main>
+
+</body>
+</html>
