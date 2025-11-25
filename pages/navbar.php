6961e12ffceb86f2905efdcd5a9be30fc49d0a9d
esnault <esnaultclement4@gmail.com>
2025-11-25 09:46:19 +0100

    fonctionnel

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,21 +1,21 @@
    <ul class="nav-links">
     <div class="nav-left z-20">
-        <li><a href="index.php">Bienvenue</a></li>
-        <li><a href="menu.php"></a> Héro</li>
+        <li><a href="index.php">Bienvenue Hero</a></li>
+        
     </div>
     <div class="nav-right">
         <?php
-        session_start();
+        //session_start();
         if(isset($_SESSION['is_logged_in'])){
             echo "<p>Bienvenue ".$_SESSION['pseudo']."</p>";
             
             //TODO ADMIN
 
-            echo '<li id="inscription"><a href="deconnexion/deconnexion.php">Se Déconnecter</a></li>';
+            echo '<li id="inscription"><a href="pages/deconnexion.php">Se Déconnecter</a></li>';
         } else{
             $text = "S'inscrire";
-            echo '<li id="inscription"><a href="Inscription.php">'.$text.'</a></li>
-            <li id="connexion"><a href="seConnecter.php">Se Connecter</a></li>';
+            echo '<li id="inscription"><a href="pages/Inscription.php">'.$text.'</a></li>
+            <li id="connexion"><a href="pages/connexion.php">Se Connecter</a></li>';
         }
         ?>
     </div>
