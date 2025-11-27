68e7fa86cf0c672189331be941b3474ce2b40381
milo <milosorelcpt@gmail.com>
2025-11-27 15:15:41 +0100

    fix(css): modifs css pour mobile notamment

--- pages/header.php
+++ pages/header.php
@@ -1,19 +1,26 @@
 <?php
-session_start();
+    session_start();
+    $root = '/sites/DungeonXplorer';
+    $_SESSION['user'] = 'Woipy';
 ?>
 
-<header class="flex-row">
-    <nav>
-        <ul>
-            <?php if (!isset($_SESSION['user'])): ?>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="decouvrir.php">Découvrir</a></li>
-                <li><a href="apropos.php">À propos</a></li>
-                <li><a href="login.php" class="btn-login">Se connecter</a></li>
+<header>
+        <div class="flex-row">         
+            <a class="button" id="logoImg" href="index.php">
+                <picture>
+                    <source media="(max-width: 768px)" srcset="<?= $root ?>/assets/logo/dungeonLogoMinLight.png">
+                    <img src="<?= $root ?>/assets/logo/dungeonLogoFullLight.png" alt="Logo" style="height:34px; width:auto;">
+                </picture>            
+            </a>
+            <a class="button navButton" href="index.php">Accueil</a>
+            <a class="button navButton" href="decouvrir.php">Découvrir</a>
+        </div>
+        <div class="flex-row">
+            <a class="button navButton" href="apropos.php">À propos</a>
+            <?php if (!isset($_SESSION['user'])): ?>     
+            <a class="button navButton" href="login.php">Se connecter</a>
             <?php else: ?>
-                <p>feur</p>
+            <a class="button" id="accountButton" href="profile.php"><?= htmlspecialchars($_SESSION['user']); ?></a>
             <?php endif; ?>
-
-        </ul>
-    </nav>
+        </div>
 </header>
