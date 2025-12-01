bfa5cb3a8cc6dcf99825f18fb3bfe96e45313174
milo <milosorelcpt@gmail.com>
2025-12-01 16:32:49 +0100

    fix(css): header termine

--- pages/header.php
+++ pages/header.php
@@ -1,26 +1,43 @@
 <?php
     session_start();
     $root = '/sites/DungeonXplorer';
-    $_SESSION['user'] = 'Woipy';
+        $_SESSION['user'] = 'Woipy';
+    
+    
 ?>
 
 <header>
         <div class="flex-row">         
-            <a class="button" id="logoImg" href="index.php">
+            <a id="logoImg" href="index.php">
                 <picture>
                     <source media="(max-width: 768px)" srcset="<?= $root ?>/assets/logo/dungeonLogoMinLight.png">
                     <img src="<?= $root ?>/assets/logo/dungeonLogoFullLight.png" alt="Logo" style="height:34px; width:auto;">
                 </picture>            
             </a>
-            <a class="button navButton" href="index.php">Accueil</a>
-            <a class="button navButton" href="decouvrir.php">Découvrir</a>
+            <a class="button flex-row" href="index.php">
+                <img src="<?= $root ?>/assets/icons/home.png" alt="Logo" style="height:34px; width:auto;">
+                <p>Accueil</p>
+            </a>
+            <a class="button flex-row" href="decouvrir.php">
+                <img src="<?= $root ?>/assets/icons/compass.png" alt="Logo" style="height:34px; width:auto;">
+                <p>Decouvrir</p>
+            </a>
         </div>
         <div class="flex-row">
-            <a class="button navButton" href="apropos.php">À propos</a>
+            <a class="button flex-row" href="apropos.php">
+                <img src="<?= $root ?>/assets/icons/info.png" alt="Logo" style="height:34px; width:auto;">
+                <p>A propos</p>
+            </a>
             <?php if (!isset($_SESSION['user'])): ?>     
-            <a class="button navButton" href="login.php">Se connecter</a>
+                <a class="button flex-row" href="login.php">
+                    <img src="<?= $root ?>/assets/icons/account.png" alt="Logo" style="height:34px; width:auto;">
+                    <p>Se connecter</p>
+                </a>
             <?php else: ?>
-            <a class="button" id="accountButton" href="profile.php"><?= htmlspecialchars($_SESSION['user']); ?></a>
+                <a class="button flex-row" href="profil.php">
+                    <img src="<?= $root ?>/assets/icons/account.png" alt="Logo" style="height:34px; width:auto;">
+                    <?= htmlspecialchars($_SESSION['user']); ?>
+                </a>
             <?php endif; ?>
         </div>
 </header>
